e2d34d861b08d93b85e3d98402588412ec7f9890
Ahsan <sbaseer25@gmail.com>
2025-09-30 09:45:33 -0400

    3rd part done!

--- src/main/java/com/pluralsight/Format.java
+++ src/main/java/com/pluralsight/Format.java
@@ -2,6 +2,15 @@ package com.pluralsight;
 
 public class Format {
     public static void main(String[] args) {
+        String first = "Bob";
+        String last = "Smith";
+        String fullName;
 
+        fullName = formatName(first, last);
+        System.out.println(fullName);
+    }
+
+    public static String formatName(String first, String last){
+        return last + ", " + first;
     }
 }
